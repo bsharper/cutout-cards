a00143aa7e472ce4fe947378b88c3174fc3e2e96
Brian Harper <bharper@gmail.com>
2020-04-23 12:14:09 -0500

    better code and stuff

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # cutout-cards
 Creates individual card images from Cards Against Humanity's Family Edition 
+
+# Running 
+
+1. Download CAH_FamilyEdition_PublicBeta_LargeCards.pdf from https://www.cardsagainsthumanityfamilyedition.com/
+2. Run `npm start` or `yarn start`
+3. Sit back and watch the magic happen. Or better yet, go on a walk while remaining properly socially distanced.
+
+# Why?
+
+I wanted to split the cards up to play around with them for personal use.
--- check_pdf_exists.js
+++ check_pdf_exists.js
@@ -2,6 +2,8 @@ const fs = require('fs')
 
 try {
     fs.accessSync('CAH_FamilyEdition_PublicBeta_LargeCards.pdf')
+    process.exitCode = 0
 } catch (err) {
-    console.log(`ERROR: Cards Against Humanity PDF not found.\n\nGo to https://www.cardsagainsthumanityfamilyedition.com/ and download the large version to this directory`)
+    console.log(`ERROR: Cards Against Humanity PDF not found.\n\n >>> Go to https://www.cardsagainsthumanityfamilyedition.com/ and download the large version to this directory <<<\n\n`)
+    process.exitCode = 1
 }
